f433cc4340f9cd7893dad45dc0c451f5481ff1ef
Santiago_Vallejos <santivallejos.01@gmail.com>
2022-09-26 15:53:50 -0300

    little changesges

--- config/formController.js
+++ config/formController.js
@@ -1,36 +1,17 @@
 import express from "express"
 const router = express.Router()
 router.use(express.urlencoded())
+import validationRules from "./validator.js"
 import nodemailer from "nodemailer"
-
-
-
-
-
-
 import validator from "express-validator"
 const { body, validationResult } = validator 
 
-const validationRules = [
-   body('firstName')
-   .notEmpty().withMessage('Debe ingresar un nombre').isLength({min: 2, max: 30}).withMessage('Su nombre debe tener entre 2 y 30 carácteres'),
-   body('lastName')
-   .notEmpty().withMessage('Debe ingresar un apellido').isLength({min: 2, max: 30}).withMessage('Su apellido debe tener entre 2 y 30 carácteres'),
-   body('email', 'Debe ingresar un mail')
-   .notEmpty().withMessage('Debe ingresar un mail').isEmail().withMessage('Debe ingresar un mail válido')
-]
-
-
-
-
-
-
+/*---------------------------------------------------------------------------*/
 
 
 router.get('/registrate', (req, res) => {
     res.render('registrate')
 })
-
 
 
 router.post('/registrate', validationRules, async (req, res) => {
@@ -42,19 +23,18 @@ router.post('/registrate', validationRules, async (req, res) => {
      res.render('registrate', {arrWarnings} )
      console.log(errors);
     } else{            
-      const { firstName, lastName, email } = req.body
-      const mailMsg = {
-        to: 'pepe@pepe.com',
-        from: email,
-        subjet: 'Mensaje desde "registrate"',
-        html: `Contacto de ${firstName} ${lastName}`
+     const { firstName, lastName, email } = req.body
+     const mailMsg = {
+     to: 'pepe@pepe.com',
+     from: email,
+     subjet: 'Mensaje desde "registrate"',
+     html: `Contacto de ${firstName} ${lastName}`
     }
     
-    
-    const transport = nodemailer.createTransport({
-      host: "smtp.mailtrap.io",
-      port: 2525,
-      auth: {
+  const transport = nodemailer.createTransport({
+     host: "smtp.mailtrap.io",
+     port: 2525,
+     auth: {
           user: "417bb401d569cc",
           pass: "57b426990076ab"
         }
@@ -62,13 +42,12 @@ router.post('/registrate', validationRules, async (req, res) => {
       
       const sendMailStatus = await transport.sendMail(mailMsg)
       let sendMailFeedback = '';
-      if (sendMailStatus.rejected.length) {
-        sendMailFeedback = 'No se pudo enviar el mensaje'
-      } else {
+      if (!sendMailStatus.length) {
         sendMailFeedback = 'El mensaje fue enviado correctamente'
-      }
+      } 
       res.render ('registrate', { message: sendMailFeedback })
     }
+    
       
     })
     
--- config/validator.js
+++ config/validator.js
@@ -1,10 +1,15 @@
-// import validator from "express-validator"
-// const { body, validationResult } = validator 
+import nodemailer from "nodemailer"
+import validator from "express-validator"
+const { body, validationResult } = validator 
 
-// const validationRules = [
-//    body('firstName')
-//    .exists().withMessage('Debe ingresar su nombre')
-   
-// ]
 
-// export default validationRules
+const validationRules = [
+    body('firstName')
+    .notEmpty().withMessage('Debe ingresar un nombre').isLength({min: 2, max: 30}).withMessage('Su nombre debe tener entre 2 y 30 carácteres'),
+    body('lastName')
+    .notEmpty().withMessage('Debe ingresar un apellido').isLength({min: 2, max: 30}).withMessage('Su apellido debe tener entre 2 y 30 carácteres'),
+    body('email', 'Debe ingresar un mail')
+    .notEmpty().withMessage('Debe ingresar un mail').isEmail().withMessage('Debe ingresar un mail válido')
+ ]
+
+ export default validationRules;
